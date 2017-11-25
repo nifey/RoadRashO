282d49cf10c9289eb8e938bff1668e548ffd7c66
Abdun Nihaal <nifey@users.noreply.github.com>
2017-11-25 23:13:15 +0530

    add some rendering code but needs fixing

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ extern crate glutin_window;
 
 mod road;
 
-use road::Road;
+use road::{Road, Coordinates, CAMERA_DEPTH};
+
+const DRAW_DISTANCE: i32 = 300;
 
 use piston::window::WindowSettings;
 use piston::event_loop::*;
@@ -24,15 +26,76 @@ fn main() {
     let mut gl = GlGraphics::new(opengl);
 
     let mut road: Road = Road::new();
+    let mut position: f32 = 0.0;
+    let mut player_x: f32 = 0.0;
 
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
         if let Some(args) = e.render_args() {
             gl.draw(args.viewport(), |c, g| {
-                use graphics::clear;
+                use graphics::*;
                 clear([1.0, 1.0, 1.0, 1.0], g);
+                let base_segment: usize = road.find_segment(position);
+                let mut max_y: f32 = 768.0;
+                for s in road.segments.iter_mut() {
+                    if s.index < DRAW_DISTANCE {
+                        s.start.project(
+                            Coordinates {
+                                x: player_x,
+                                y: 0.0,
+                                z: position,
+                            },
+                            1024,
+                            768,
+                        );
+                        s.end.project(
+                            Coordinates {
+                                x: player_x,
+                                y: 0.0,
+                                z: position,
+                            },
+                            1024,
+                            768,
+                        );
+                        if s.start.camera.z <= CAMERA_DEPTH || s.end.screen.y >= max_y {
+                            continue;
+                        }
 
-                let position: i32 = 0;
+                        let polygon1: Polygon = Polygon::new(s.color);
+                        let vertices =vec![[s.start.screen.x as f64, s.start.screen.y as f64],
+                            [
+                                (s.start.screen.x + s.start.screen.z) as f64,
+                                s.start.screen.y as f64
+                            ],
+                            [
+                                (s.end.screen.x + s.end.screen.z) as f64,
+                                s.end.screen.y as f64
+                            ],[s.end.screen.x as f64, s.end.screen.y as f64],
+[s.start.screen.x as f64, s.start.screen.y as f64]
+                        ] ;
+                            println!("{:?}",vertices);
+/*                        vertices.push([s.start.screen.x as f64, s.start.screen.y as f64]);
+                        vertices.push(
+                            [
+                                (s.start.screen.x + s.start.screen.z) as f64,
+                                s.start.screen.y as f64,
+                            ],
+                        );
+                        vertices.push(
+                            [
+                                (s.end.screen.x + s.end.screen.z) as f64,
+                                s.end.screen.y as f64,
+                            ],
+                        );
+                        vertices.push([s.end.screen.x as f64, s.end.screen.y as f64]);
+                        vertices.push([s.start.screen.x as f64, s.start.screen.y as f64]);
+ */                       let transform = c.transform.trans(0.0, 0.0);
+                        polygon(s.color, &vertices[..], transform, g);
+
+
+                        max_y = s.end.screen.y;
+                    }
+                }
             });
         }
         if let Some(args) = e.update_args() {}
--- src/road.rs
+++ src/road.rs
@@ -2,50 +2,47 @@ extern crate graphics;
 
 use graphics::types::Color;
 
+const ROAD_WIDTH: i32 = 2000;
 const SEGMENT_LENGTH: i32 = 200;
 const RUMBLE_LENGTH: i32 = 3;
 const DARK_COLOR: Color = [0.2, 0.2, 0.2, 1.0];
 const LIGHT_COLOR: Color = [0.8, 0.8, 0.8, 1.0];
 
-struct Coordinates {
-    x: i32,
-    y: i32,
-    z: i32,
+const LANES: i32 = 3;
+pub const CAMERA_DEPTH: f32 = 0.8;
+
+pub struct Coordinates {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
 }
 
-struct Point {
-    world: Coordinates,
-    camera: Coordinates,
-    screen: Coordinates,
+pub struct Point {
+    pub world: Coordinates,
+    pub camera: Coordinates,
+    pub screen: Coordinates,
 }
 impl Point {
-    fn project(
-        &mut self,
-        camera: Coordinates,
-        camera_depth: i32,
-        width: i32,
-        height: i32,
-        road_width: i32,
-    ) {
+    pub fn project(&mut self, camera: Coordinates, width: i32, height: i32) {
         self.camera.x = self.world.x - camera.x;
         self.camera.y = self.world.y - camera.y;
         self.camera.z = self.world.z - camera.z;
-        let scale: i32 = camera_depth / self.camera.z;
-        self.screen.x = width / 2 + scale * self.camera.x * width / 2;
-        self.screen.y = height / 2 + scale * self.camera.y * height / 2;
-        self.screen.z = scale * road_width * width / 2;
+        let scale: f32 = CAMERA_DEPTH / self.camera.z;
+        self.screen.x = width as f32 / 2.0 + (scale * self.camera.x * width as f32 / 2.0);
+        self.screen.y = height as f32 / 2.0 + (scale * self.camera.y * height as f32 / 2.0);
+        self.screen.z = scale * ROAD_WIDTH as f32 * width as f32 / 2.0;
     }
 }
 
-struct Segment {
-    index: i32,
-    color: Color,
-    start: Point,
-    end: Point,
+pub struct Segment {
+    pub index: i32,
+    pub color: Color,
+    pub start: Point,
+    pub end: Point,
 }
 
 pub struct Road {
-    segments: Vec<Segment>,
+    pub segments: Vec<Segment>,
 }
 
 impl Road {
@@ -61,27 +58,43 @@ impl Road {
                 },
                 start: Point {
                     world: Coordinates {
-                        x: 0,
-                        y: 0,
-                        z: n * SEGMENT_LENGTH,
+                        x: 0.0,
+                        y: 0.0,
+                        z: (n * SEGMENT_LENGTH) as f32,
                     },
-                    camera: Coordinates { x: 0, y: 0, z: 0 },
-                    screen: Coordinates { x: 0, y: 0, z: 0 },
+                    camera: Coordinates {
+                        x: 0.0,
+                        y: 0.0,
+                        z: 0.0,
+                    },
+                    screen: Coordinates {
+                        x: 0.0,
+                        y: 0.0,
+                        z: 0.0,
+                    },
                 },
                 end: Point {
                     world: Coordinates {
-                        x: 0,
-                        y: 0,
-                        z: n * SEGMENT_LENGTH,
+                        x: 0.0,
+                        y: 0.0,
+                        z: (n * SEGMENT_LENGTH) as f32,
                     },
-                    camera: Coordinates { x: 0, y: 0, z: 0 },
-                    screen: Coordinates { x: 0, y: 0, z: 0 },
+                    camera: Coordinates {
+                        x: 0.0,
+                        y: 0.0,
+                        z: 0.0,
+                    },
+                    screen: Coordinates {
+                        x: 0.0,
+                        y: 0.0,
+                        z: 0.0,
+                    },
                 },
             });
         }
         Road { segments: segments }
     }
-    fn find_segment(&self, z: i32) -> &Segment {
-        &self.segments[(z / SEGMENT_LENGTH) as usize % self.segments.len()]
+    pub fn find_segment(&self, z: f32) -> usize {
+        (z / SEGMENT_LENGTH as f32) as usize % self.segments.len()
     }
 }
